dbb065f450adfcdf77ac6057371995fa6f60684c
kaike <kaike@DESKTOP-UQ6TPQQ>
2023-08-30 19:45:49 -0300

    testes de funcionalidades do relogio

--- src/main/test/java/com/example/RelogioTest.java
+++ src/main/test/java/com/example/RelogioTest.java
@@ -62,5 +62,73 @@ public class RelogioTest {
         relogio.setMinuto(10);
         relogio.setSegundo(59);
         assertEquals("12:10:59", relogio.toString());
+        
+    }
+    
+    @Test
+    void testReiniciarMeiaNoite() {
+        Relogio relogio = new Relogio();
+        relogio.setHora(10);
+        relogio.setMinuto(30);
+        relogio.setSegundo(45);
+        
+        relogio.reiniciarMeiaNoite();
+        
+        assertEquals(0, relogio.getHora());
+        assertEquals(0, relogio.getMinuto());
+        assertEquals(0, relogio.getSegundo());
+    }
+
+    @Test
+    void testMarcarIntervalo() {
+        Relogio relogio = new Relogio();
+        
+        String intervalo = relogio.marcarIntervalo(10, 30, 0, 11, 15, 30);
+        
+        assertEquals("00:45:30", intervalo);
+    }
+
+    @Test
+    void testImprimeFormato24h() {
+        Relogio relogio = new Relogio();
+        relogio.setHora(15);
+        relogio.setMinuto(40);
+        relogio.setSegundo(5);
+        
+        String formato24h = relogio.imprimeFormato24h();
+        
+        assertEquals("15:40:05", formato24h);
+    }
+
+    @Test
+    void testImprimeFormatoAMPM() {
+        Relogio relogioAM = new Relogio();
+        relogioAM.setHora(10);
+        relogioAM.setMinuto(30);
+        relogioAM.setSegundo(0);
+        
+        String formatoAM = relogioAM.imprimeFormatoAMPM();
+        
+        assertEquals("10:30:00 AM", formatoAM);
+        
+        Relogio relogioPM = new Relogio();
+        relogioPM.setHora(20);
+        relogioPM.setMinuto(45);
+        relogioPM.setSegundo(30);
+        
+        String formatoPM = relogioPM.imprimeFormatoAMPM();
+        
+        assertEquals("08:45:30 PM", formatoPM);
+    }
+
+    @Test
+    void testAtualizaTempo() {
+        Relogio relogio = new Relogio();
+        
+        relogio.atualizaTempo(8, 15, 30);
+        
+        assertEquals(8, relogio.getHora());
+        assertEquals(15, relogio.getMinuto());
+        assertEquals(30, relogio.getSegundo());
     }
 }
