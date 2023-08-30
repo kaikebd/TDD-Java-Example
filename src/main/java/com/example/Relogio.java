0f6ef72ca1753b8d851520dfdea975c7f9b908e1
kaike <kaike@DESKTOP-UQ6TPQQ>
2023-08-30 19:34:27 -0300

    metodos relogio

--- src/main/java/com/example/Relogio.java
+++ src/main/java/com/example/Relogio.java
@@ -2,4 +2,53 @@ package com.example;
 
 public class Relogio {
 
+    private int hora;
+    private int minuto;
+    private int segundo;
+
+    public Relogio() {
+        this.hora = 0;
+        this.minuto = 0;
+        this.segundo = 0;
+    }
+
+    public void setHora(int hora) {
+        if (hora < 0 || hora > 23) {
+            throw new IllegalArgumentException("Hora inválida.");
+        }
+        this.hora = hora;
+    }
+
+    public void setMinuto(int minuto) {
+        if (minuto < 0 || minuto > 59) {
+            throw new IllegalArgumentException("Minuto inválido.");
+        }
+        this.minuto = minuto;
+    }
+
+    public void setSegundo(int segundo) {
+        if (segundo < 0 || segundo > 59) {
+            throw new IllegalArgumentException("Segundo inválido.");
+        }
+        this.segundo = segundo;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%02d:%02d:%02d", this.hora, this.minuto, this.segundo);
+    }
+
+    public int getHora() {
+        return this.hora;
+    }
+
+    public int getSegundo() {
+        return this.segundo;
+    }
+
+    public int getMinuto() {
+        return this.minuto;
+    }
 }
+
+
