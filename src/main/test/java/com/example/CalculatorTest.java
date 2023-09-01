05efe8de8aab07efc9a2701fe15facbe6ba909d2
kaikebd <102492267+kaikebd@users.noreply.github.com>
2023-08-31 23:36:00 -0300

    Create CalculatorTest.java

---
+++ src/main/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int result = calculator.sum(2, 3);
+        assertEquals(5, result);
+    }
+}
